ac0ebe22b1ae9cb9af943fc34551e543db5ee8e4
bradgerb <77023448+bradgerb@users.noreply.github.com>
2026-05-09 18:29:34 -0400

    Add functions to admin only view

--- db/queries.js
+++ db/queries.js
@@ -39,7 +39,7 @@ async function becomeAdmin(username) {
 
 async function getMessages() {
   const { rows } = await pool.query(
-    "SELECT message, username, trim(TO_CHAR(date, 'FMDay, DD Mon YYYY HH12:MI AM')) AS date FROM messages \
+    "SELECT message_id, users.user_id, message, username, trim(TO_CHAR(date, 'FMDay, DD Mon YYYY HH12:MI AM')) AS date FROM messages \
     JOIN users ON users.user_id = messages.user_id \
     ORDER BY message_id"
   );
